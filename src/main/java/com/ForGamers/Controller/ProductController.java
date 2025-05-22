ba9349b32218d12c5f6ccb1b082f5dc8359a89c2
Tiago Jaen <tiagojaen123@gmail.com>
2025-05-22 16:55:35 -0300

    Páginas index e inicio de sesión(incompletas) y clases de Product
    Estos archivos los estabamos trabajando en un repositorio secundario para testear la conexión de endpoints y decidir el diseño de la página, para no hacer tantos cambios innecesarios en el repositorio principal decidimos usar otro. Ya decidido el diseño comenzamos con la estructura del backend desde este repositorio que es el principal.

---
+++ src/main/java/com/ForGamers/Controller/ProductController.java
@@ -0,0 +1,54 @@
+package com.ForGamers.Controller;
+
+import com.ForGamers.Model.Product;
+import com.ForGamers.Service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@Tag(name = "products", description = "Operaciones relacionadas a los productos.")
+public class ProductController {
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    //METODOS
+    @Operation(summary = "Obtener listado de productos.", description = "Devuelve una lista de todos los productos.")
+    @GetMapping
+    public List<Product> listProducts() {
+        return productService.listProducts();
+    }
+
+    @Operation(summary = "Agregar un producto.", description = "No incluir id al agregar un producto.")
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+
+    @Operation(summary = "Eliminar un producto por id.")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        return productService.deleteProduct(id);
+    }
+
+    @Operation(summary = "Obtener producto por id.")
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable Long id){
+        return productService.getById(id);
+    }
+
+    @Operation(summary = "Editar un producto.")
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> modifyProduct(@PathVariable Long id, @RequestBody Product updatedProduct) {
+        return productService.modifyProduct(id, updatedProduct);
+    }
+
+
+}
---
+++ src/main/java/com/ForGamers/Repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.ForGamers.Repository;
+
+import com.ForGamers.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
