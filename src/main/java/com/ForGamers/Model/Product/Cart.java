290be1908aa6d63ec85f545692e719f3a7a2e502
unknown <tomasrivara23.7@gmail.com>
2025-06-05 11:32:57 -0300

    Creacion de clases de carrito

---
+++ src/main/java/com/ForGamers/Model/Product/Cart.java
@@ -0,0 +1,27 @@
+package com.ForGamers.Model.Product;
+
+import com.ForGamers.Model.User.Client;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(example = "")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+}
--- src/main/java/com/ForGamers/Repository/User/UserRepository.java
+++ src/main/java/com/ForGamers/Repository/User/UserRepository.java
@@ -4,6 +4,8 @@ import com.ForGamers.Model.User.User;
 import com.ForGamers.Model.User.UserDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 public interface UserRepository<T extends User> extends JpaRepository<T, Long> {
@@ -11,5 +13,5 @@ public interface UserRepository<T extends User> extends JpaRepository<T, Long> {
     Optional<T> getByUsername(String username);
 
     @Query("SELECT u FROM #{#entityName} u WHERE u.email = :email")
-    Optional<T> getByEmail(String username);
+    Optional<T> getByEmail(String email);
 }
