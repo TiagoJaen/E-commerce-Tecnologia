f2bcfc4d3558455bf6afc32de7daa25c107f1bae
TiagoJaen <tiagojaen123@gmail.com>
2025-06-09 21:43:42 -0300

    UserController para obtener usuario logeado

---
+++ src/main/java/com/ForGamers/Controller/User/UserController.java
@@ -0,0 +1,26 @@
+package com.ForGamers.Controller.User;
+
+import com.ForGamers.Model.User.User;
+import com.ForGamers.Service.User.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private UserService userService;
+    @GetMapping
+    public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
+        User user = (User) userService.getByUsername(userDetails.getUsername()).get();
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<?> updateCurrentUser(@RequestBody User updatedUser) {
+        // Actualizar datos del usuario autenticado
+        userService.modify(updatedUser.getId(), updatedUser);
+        return ResponseEntity.ok("Datos actualizados");
+    }
+}
--- src/main/java/com/ForGamers/Repository/User/UserRepository.java
+++ src/main/java/com/ForGamers/Repository/User/UserRepository.java
@@ -1,10 +1,8 @@
 package com.ForGamers.Repository.User;
 
 import com.ForGamers.Model.User.User;
-import com.ForGamers.Model.User.UserDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
