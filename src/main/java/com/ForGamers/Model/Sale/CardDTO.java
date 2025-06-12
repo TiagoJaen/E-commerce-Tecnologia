2d6d777cee59f4746b1ff6f05bd68c7be228b005
rivatoDev <tomasrivara23.7@gmail.com>
2025-06-12 12:24:56 -0300

    Modificacion CardDTO

--- src/main/java/com/ForGamers/Model/Sale/CardDTO.java
+++ src/main/java/com/ForGamers/Model/Sale/CardDTO.java
@@ -1,5 +1,30 @@
 package com.ForGamers.Model.Sale;
 
-public class CardDTO {
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class CardDTO {
+    private Long id;
+
+    @Size(max = 20, message = "El titular debe ser de maximo 20 caracteres")
+    private String holder;
+
+    @Pattern(regexp = "\\d{16}", message = "El numero de tarjeta debe ser de 16 numeros")
+    private String number;
+
+    private LocalDate fecha;
+
+    @Size(min = 3, max = 3, message = "El codigo de seguridad debe ser de 3 numeros enteros")
+    private Integer cvv;
 }
