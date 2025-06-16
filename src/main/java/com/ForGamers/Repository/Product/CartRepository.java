6ea6ee37534af5fdfb72bfd1459e88042a2c9ec3
AgustinP27 <agustinprvnzn@gmail.com>
2025-06-16 20:41:34 -0300

    Cart -> CartEntry in CartRepository

--- src/main/java/com/ForGamers/Repository/Product/CartRepository.java
+++ src/main/java/com/ForGamers/Repository/Product/CartRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<CartEntry, Long >{
-    @Query("SELECT c.product FROM Cart c WHERE c.client.id = :client_Id")
+    @Query("SELECT c.product FROM CartEntry c WHERE c.client.id = :client_Id")
     List<Product> findProductsByClientId(@Param("client_Id")Long client_Id);
 
-    @Query("SELECT c FROM Cart c WHERE c.product.id = :product_Id AND c.client.id = :client_Id")
+    @Query("SELECT c FROM CartEntry c WHERE c.product.id = :product_Id AND c.client.id = :client_Id")
     Optional<CartEntry> findById(@Param("client_Id")Long client_Id,
                                  @Param("product_Id")Long product_Id);
 
